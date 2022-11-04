faffa1a1065f6fecc8825fae4878b6a189078f0e
LaszloKenesei <keneseilaca44@gmail.com>
2022-11-04 16:47:57 +0100

    gui1

--- main.py
+++ main.py
@@ -2,32 +2,20 @@ import functions
 from tkinter import *
 root = Tk()
 
-root.geometry('600x400')
+root.geometry('700x400')
+canvas = Canvas(root, width=700, height=400, bg='white')
+canvas.pack()
 root.title('YouTube downloader')
-#Label(root, text="Paste the video link below", font=25).place(x=15, y=15)
-Button(root,width=20, height=32, borderwidth=1,text="♬ Audio").place(x=200, y=60)
-Button(root, text="▶ Video", bd=1).place(x=400, y=60)
-Text(root).place (x=300, y=100)
+Button(root, borderwidth=0, text="♬  Audio", font=('Calibri_Light'), bg='#e6e6e6', fg='#333333').place(x=250, y=3, width=100, height=65)
+Button(root, borderwidth=0, text="▶  Video", font=('Calibri_Light'), bg='#e6e6e6', fg='#333333').place(x=350, y=3, width=100, height=65)
+Label(root, text='URL', anchor="e", fg='#404040', bg='white', justify='left', font=22).place(x=40, y=170, width=100)
+Label(root, text='Resolution', anchor="e", fg='#404040', bg='white', justify=RIGHT, font=22).place(x=40, y=225, width=100)
+Text(root, border=1).place (x=150, y=170,  height=36, width=460)
+Text(root, border=1).place (x=150, y=225, height=36, width=460)
+canvas.create_rectangle(0, 0, 700, 70, fill='#e6e6e6', outline="#cccccc")
 
 root.mainloop()
-'''import tkinter as tk
 
-root = tk.Tk()
-root.title("YouTube downloader")
-canvas = tk.Canvas(root, width=800, height=400)
-canvas.grid(columnspan=3, rowspan=3)
-lbl1 = tk.Label(root, text="Paste the video link below", font=25)
-lbl1.grid(column=0, row=0)
-lbl2 = tk.Label(root, text="Paste the video link below", font=25)
-lbl2.grid(column=1, row=0)
-mp3bttn = tk.Button(root, text="♬ Audio")
-mp3bttn.grid(column=0, row=1)
-mp4bttn = tk.Button(root, text="▶ Video")
-mp4bttn.grid(column=1, row=1)
-
-
-
-root.mainloop()'''
 
 
 
